92fd3516a80a90ce5bfd9acade4b7db04f163fba
lpaissanidis-source <lpaissanidis@gmail.com>
2026-06-09 11:01:43 +0200

    Fix: add basePath to Sanity Studio config

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,6 +8,7 @@ import { schemaTypes } from './src/sanity/schemas'
 export default defineConfig({
   name: 'tochi-portfolio',
   title: 'Portfolio de Tochi',
+  basePath: '/studio',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   plugins: [structureTool(), visionTool()],
